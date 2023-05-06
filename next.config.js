8b6bd35f11d3e45f6883a4278cafb7a128d2fe87
freetoknow <freewarrior622@gmail.com>
2023-05-06 23:55:22 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/github.io',
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    unoptimized: true,
-  }
+  
 }
 
 module.exports = nextConfig
