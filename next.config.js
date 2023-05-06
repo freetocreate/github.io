6bf63b87766a1477be0c83289ac60605a926cd28
freetoknow <freewarrior622@gmail.com>
2023-05-06 23:10:41 +0900

    add

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  output: 'export',
+  basePath: '/github.io',
+  experimental: {
+    appDir: true,
+  }
 }
 
 module.exports = nextConfig
-module.exports = {
-  repository: 'github.io',
-  
-};
-
