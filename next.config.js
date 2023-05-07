5268b28a9b11bf389fec3ea63216edbd13182ee2
freetoknow <freewarrior622@gmail.com>
2023-05-07 10:51:13 +0900

    ass

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   
-  output: 'export',
+
   assetPrefix: isProd ? '/github.io/' : '',
   headers: async () => [
       {
