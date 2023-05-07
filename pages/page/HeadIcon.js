cacf6cb89e216b36a98b2f03eb6c3bc2e94d3319
freetoknow <freewarrior622@gmail.com>
2023-05-07 10:24:28 +0900

    add

--- pages/page/HeadIcon.js
+++ pages/page/HeadIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Image from 'next/image';
+
 const HeadIcon = ({ src, alt }) => {
-  return <Image src={src} alt={alt} loader="default" className="w-6 h-6 rounded-full" />;
+  return <img src={src} alt={alt}  className="w-6 h-6 rounded-full" />;
   
 };
 
