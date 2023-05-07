289800da17f8f3598fe0cdff32d89f9d2f74b7be
freetoknow <freewarrior622@gmail.com>
2023-05-07 10:54:35 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  
 
-  assetPrefix: isProd ? '/github.io/' : '',
+
+  assetPrefix: isProd ? '/' : '',
   headers: async () => [
       {
         source: '/(.*)',
