c786ee860e6d60383a8bd4d09cfcfa630af19e09
freetoknow <freewarrior622@gmail.com>
2023-05-07 10:49:31 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   
   output: 'export',
   assetPrefix: isProd ? '/github.io/' : '',
+  headers: async () => [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'interest-cohort=()',
+          },
+        ],
+      },
+    ]
 }
 
 module.exports = nextConfig
